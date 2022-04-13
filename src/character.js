28670fee0b00964c334d7265f3919ca92891b1b6
wcjameson <williamjameson90@gmail.com>
2022-04-13 15:09:05 -0700

    WIP
    Co-authored-by: jakechaley <jakechaley@users.noreply.github.com>

--- src/character.js
+++ src/character.js
@@ -1,37 +1,37 @@
-import $ from 'jquery'; 
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+// import $ from 'jquery'; 
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './css/styles.css';
 
-// This function stores our state.
+// // This function stores our state.
 
-const storeState = () => {
-  let currentState = {};
-  return (stateChangeFunction = state => state) => {
-    const newState = stateChangeFunction(currentState);
-    currentState = {...newState};
-    return newState;
-  }
-}
+// export const storeState = () => {
+//   let currentState = {};
+//   return (stateChangeFunction = state => state) => {
+//     const newState = stateChangeFunction(currentState);
+//     currentState = {...newState};
+//     return newState;
+//   }
+// }
 
-const stateControl = storeState();
+// export const stateControl = storeState();
 
-//This is a function factory. We can easily create functions that alter a character's attributes (Level, Strength, Dexterity, intelligence)
+// //This is a function factory. We can easily create functions that alter a character's attributes (Level, Strength, Dexterity, intelligence)
 
-const changeState = (prop) => {
-  return (value) => {
-    return (state) => ({
-      ...state,
-      [prop] : (state[prop] || 0) + value
-    })
-  }
-}
+// export const changeState = (prop) => {
+//   return (value) => {
+//     return (state) => ({
+//       ...state,
+//       [prop] : (state[prop] || 0) + value
+//     })
+//   }
+// }
 
-let characterWizard = {};
-let characterWarrior = {};
-let characterPrisoner = {};
+// let characterWizard = {};
+// let characterWarrior = {};
+// let characterPrisoner = {};
 
-const levelUp = changeState("level")(1);
-const levelUpStr = changeState("strength")(1);
-const levelUpDex = changeState("dexterity")(1);
-const levelUpInt = changeState("intelligence")(1);
+// export const levelUp = changeState("level")(1);
+// export const levelUpStr = changeState("strength")(1);
+// export const levelUpDex = changeState("dexterity")(1);
+// export const levelUpInt = changeState("intelligence")(1);
--- src/index.js
+++ src/index.js
@@ -1,26 +1,103 @@
-import 'character.js';
+// import 'character.js';
 import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 
-$(document).readyState(function() {
+// This function stores our state.
 
-  $("#levelUp").on("click", function() {
-    const newState = stateControl(levelUp);
-    $("#level-value").text(`Level: ${newState.level}`);
-  });
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    // eslint-disable-next-line 
+    currentState = {...newState};
+    return newState;
+  }
+}
 
-  $("#levelUpStr").on("click", function() {
-    const newState = stateControl(levelUpStr);
-    $("levelStr-value").text(`Level: ${newState.strength}`);
-  });
+const stateControl = storeState();
 
-  $("#levelUpDex").on("click", function() {
-    const newState = stateControl(levelUpDex);
-    $("levelDex-value").text(`Level: ${newState.dexterity}`);
-  });
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
 
-  $("#levelUpInt").on("click", function() {
-    const newState = stateControl(levelUpInt);
-    $("levelInt-value").text(`Level: ${newState.intelligence}`);
-  });
+const storeListState = () => {
+  let currentState = [];
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = [...newState];
+    return newState;
+  }
+}
+
+const changeListState = (character) => {
+  return (state) => ([
+    ...state,
+    character
+  ])
+}
+
+//This is a function factory. We can easily create functions that alter a character's attributes (Level, Strength, Dexterity, intelligence)
+
+// let characterWizard = {};
+// let characterWarrior = {};
+// let characterPrisoner = {};
+
+const levelUp = changeState("level")(1);
+const levelUpStr = changeState("strength")(1);
+const levelUpDex = changeState("dexterity")(1);
+const levelUpInt = changeState("intelligence")(1);
+
+
+$(document).ready(function() {
+
+  const listControl = storeListState();
+
+  $('#new-character').click(function() {
+    const characterControl = storeState();
+    const addCharacter = changeListState(characterControl);
+    const newList = listControl(addCharacter);
+    $("#output").append(`
+    <div>
+      <p id="level-value-${newList.length - 1}">0</p>
+      <button class="btn-primary" id="levelUp-${newList.length - 1}" class="levelUp">Level Up!</button>
+    </div>
+  `);
 });
 
+  $("body").on("click",".levelUp",function() {
+    const id = parseInt(this.id.slice(5));
+    const stateControl = listControl()[id];
+    const newState = stateControl(levelUp);
+    $(`#level-value-${id}`).text(`Level: ${newState.level}`);
+  });
+
+
+});
+
+
+// $(`"#levelUp-${newList.length - 1}"`).on("click", function() {
+//   const newState = stateControl(levelUp);
+//   $(`#level-value-${newList.length - 1}"`).text(`Level: ${newState.level}`);
+// });
+
+  // $("#levelUpStr").on("click", function() {
+  //   const newState = stateControl(levelUpStr);
+  //   $("#levelStr-value").text(`Strength: ${newState.strength}`);
+  // });
+
+  // $("#levelUpDex").on("click", function() {
+  //   const newState = stateControl(levelUpDex);
+  //   $("#levelDex-value").text(`Dexterity: ${newState.dexterity}`);
+  // });
+
+  // $("#levelUpInt").on("click", function() {
+  //   const newState = stateControl(levelUpInt);
+  //   $("#levelInt-value").text(`Intelligence: ${newState.intelligence}`);
+  // });
